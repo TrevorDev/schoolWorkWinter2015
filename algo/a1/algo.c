499b8c2088c5719d2eed9f903e8b5912309046de
Trevor <trevorjbaron@gmail.com>
2015-01-19 01:23:09 -0500

    use double precision

--- algo/a1/algo.c
+++ algo/a1/algo.c
@@ -2,24 +2,24 @@
 #include <math.h>
 #include <stdlib.h>
 
-long long fact(long long x){
-    if(x==0||x==1){
+long double fact(long double x){
+    if(x==0 || x==1){
         return x;
     }else{
         return x*fact(x-1);
     }
 }
 
-double logt(double f){
+long double logt(long double f){
     return (log2(f));
 }
 
-double nlogRes(long long c, long long mid){
+long double nlogRes(long double c, long double mid){
     return -(mid*logt(mid)-c);
 }
 
-double factRes(long long c, long long mid){
-    return ((double)c/(double)fact(mid))-1;
+long double factRes(long double c, long double mid){
+    return ((long double)c/(long double)fact(mid))-1;
 }
 
 int main(int argc, char **argv)
@@ -28,15 +28,15 @@ int main(int argc, char **argv)
         printf("ALGO\nTo run this program use ALGO X\neg. ./ALGO 24\n");
         return 0;
     }
-    //double c = strtof(argv[1], NULL);
-    //long long c = 24;
-    char * e;
-    long long c = strtoll(argv[1], &e, 0);
-    //printf("%lld\n", c);
-    // double low = 0;
-    // double high = 32000;
-    // double mid = 0;
-    // double res = 0;
+    //long double c = strtof(argv[1], NULL);
+    //long double c = 24;
+    //char * e;
+    long double c = atof(argv[1]);
+    //printf("%LF\n", c);
+    // long double low = 0;
+    // long double high = 32000;
+    // long double mid = 0;
+    // long double res = 0;
     
     // while(high - low > 0.0001){
     //     mid = (low+high)/2;
@@ -47,17 +47,17 @@ int main(int argc, char **argv)
     //         high = mid;
     //     }
     // }
-    // printf("%f\n%f\n%f\n", mid, mid*logt(mid), c);
+    // printf("%LF\n%LF\n%LF\n", mid, mid*logt(mid), c);
 
-    long long high =  9223372036854775807;
-    long long low = 0;
-    long long mid = 0;
-    long long ans = 0;
-    double res = 0;
+    long double high =  9223372036854775807;
+    long double low = 0;
+    long double mid = 0;
+    long double ans = 0;
+    long double res = 0;
     while((high - low) > 1){
-        mid = (high+low)/2;
+        mid = floorl((high+low)/2);
         res = nlogRes(c, mid);
-        //printf("%d %d %d %f\n", high, low, fact(mid), res);
+        //printf("%d %d %d %LF\n", high, low, fact(mid), res);
         if(res > 0){
             low = mid;
         }else{
@@ -69,18 +69,18 @@ int main(int argc, char **argv)
     }else{
         ans = low;
     }
-    //printf("n1 = %lld %lld %lld %f\n", ans, high, low, ans*logt(ans)-c);
-    printf("n1 = %lld\n", ans);
+    //printf("n1 = %LF %LF %LF %LF\n", ans, high, low, ans*logt(ans)-c);
+    printf("n1 = %LF\n", ans);
 
-    high =  20;
+    high =  50;
     low = 0;
     mid = 0;
     ans = 0;
     res = 0;
     while((high - low) > 1){
-        mid = (high+low)/2;
+        mid = floorl((high+low)/2);
         res = factRes(c, mid);
-        //printf("%lld %lld %lld %f\n", high, low, fact(mid), res);
+        //printf("%LF %LF %LF %LF\n", high, low, fact(mid), res);
         if(res > 0){
             low = mid;
         }else{
@@ -92,12 +92,8 @@ int main(int argc, char **argv)
     }else{
         ans = low;
     }
-
-    //printf("n2 = %lld %lld %lld %f\n", ans, high, low, ((double)c/(double)fact(ans))-1);
-    printf("n2 = %lld\n", ans);
-    //printf("%d\n", mid);
-
-
+    //printf("n2 = %LF %LF %LF %LF\n", ans, high, low, ((long double)c/(long double)fact(ans))-1);
+    printf("n2 = %LF\n", ans);
     return 0;
 }
 
